a373d08f9b022d1ffe3f96486026f04d82728584
victoryang00 <yangyiwei2000@gmail.com>
2023-12-15 17:40:03 -0800

    add multi-thread bc

--- src/bc.cc
+++ src/bc.cc
@@ -15,6 +15,7 @@
 #include "sliding_queue.h"
 #include "timer.h"
 #include "util.h"
+#include "omp.h"
 
 
 /*
@@ -49,111 +50,112 @@ typedef double CountT;
 
 
 void PBFS(const Graph &g, NodeID source, pvector<CountT> &path_counts,
-    Bitmap &succ, vector<SlidingQueue<NodeID>::iterator> &depth_index,
-    SlidingQueue<NodeID> &queue) {
-  pvector<NodeID> depths(g.num_nodes(), -1);
-  depths[source] = 0;
-  path_counts[source] = 1;
-  queue.push_back(source);
-  depth_index.push_back(queue.begin());
-  queue.slide_window();
-  const NodeID* g_out_start = g.out_neigh(0).begin();
-  #pragma omp parallel
-  {
-    NodeID depth = 0;
-    QueueBuffer<NodeID> lqueue(queue);
-    while (!queue.empty()) {
-      depth++;
-      #pragma omp for schedule(dynamic, 64) nowait
-      for (auto q_iter = queue.begin(); q_iter < queue.end(); q_iter++) {
-        NodeID u = *q_iter;
-        for (NodeID &v : g.out_neigh(u)) {
-          if ((depths[v] == -1) &&
-              (compare_and_swap(depths[v], static_cast<NodeID>(-1), depth))) {
-            lqueue.push_back(v);
-          }
-          if (depths[v] == depth) {
-            succ.set_bit_atomic(&v - g_out_start);
-            #pragma omp atomic
-            path_counts[v] += path_counts[u];
-          }
+          Bitmap &succ, vector<SlidingQueue<NodeID>::iterator> &depth_index,
+          SlidingQueue<NodeID> &queue) {
+    pvector<NodeID> depths(g.num_nodes(), -1);
+    depths[source] = 0;
+    path_counts[source] = 1;
+    queue.push_back(source);
+    depth_index.push_back(queue.begin());
+    queue.slide_window();
+    const NodeID* g_out_start = g.out_neigh(0).begin();
+#pragma omp parallel num_threads(8)
+    {
+        printf("Hello World... from thread = %d\n", omp_get_thread_num());
+        NodeID depth = 0;
+        QueueBuffer<NodeID> lqueue(queue);
+        while (!queue.empty()) {
+            depth++;
+#pragma omp for schedule(dynamic, 64) nowait
+            for (auto q_iter = queue.begin(); q_iter < queue.end(); q_iter++) {
+                NodeID u = *q_iter;
+                for (NodeID &v : g.out_neigh(u)) {
+                    if ((depths[v] == -1) &&
+                        (compare_and_swap(depths[v], static_cast<NodeID>(-1), depth))) {
+                        lqueue.push_back(v);
+                    }
+                    if (depths[v] == depth) {
+                        succ.set_bit_atomic(&v - g_out_start);
+#pragma omp atomic
+                        path_counts[v] += path_counts[u];
+                    }
+                }
+            }
+            lqueue.flush();
+#pragma omp barrier
+#pragma omp single
+            {
+                depth_index.push_back(queue.begin());
+                queue.slide_window();
+            }
         }
-      }
-      lqueue.flush();
-      #pragma omp barrier
-      #pragma omp single
-      {
-        depth_index.push_back(queue.begin());
-        queue.slide_window();
-      }
     }
-  }
-  depth_index.push_back(queue.begin());
+    depth_index.push_back(queue.begin());
 }
 
 
 pvector<ScoreT> Brandes(const Graph &g, SourcePicker<Graph> &sp,
                         NodeID num_iters) {
-  Timer t;
-  t.Start();
-  pvector<ScoreT> scores(g.num_nodes(), 0);
-  pvector<CountT> path_counts(g.num_nodes());
-  Bitmap succ(g.num_edges_directed());
-  vector<SlidingQueue<NodeID>::iterator> depth_index;
-  SlidingQueue<NodeID> queue(g.num_nodes());
-  t.Stop();
-  PrintStep("a", t.Seconds());
-  const NodeID* g_out_start = g.out_neigh(0).begin();
-  for (NodeID iter=0; iter < num_iters; iter++) {
-    NodeID source = sp.PickNext();
-    cout << "source: " << source << endl;
+    Timer t;
     t.Start();
-    path_counts.fill(0);
-    depth_index.resize(0);
-    queue.reset();
-    succ.reset();
-    PBFS(g, source, path_counts, succ, depth_index, queue);
+    pvector<ScoreT> scores(g.num_nodes(), 0);
+    pvector<CountT> path_counts(g.num_nodes());
+    Bitmap succ(g.num_edges_directed());
+    vector<SlidingQueue<NodeID>::iterator> depth_index;
+    SlidingQueue<NodeID> queue(g.num_nodes());
     t.Stop();
-    PrintStep("b", t.Seconds());
-    pvector<ScoreT> deltas(g.num_nodes(), 0);
-    t.Start();
-    for (int d=depth_index.size()-2; d >= 0; d--) {
-      #pragma omp parallel for schedule(dynamic, 64)
-      for (auto it = depth_index[d]; it < depth_index[d+1]; it++) {
-        NodeID u = *it;
-        ScoreT delta_u = 0;
-        for (NodeID &v : g.out_neigh(u)) {
-          if (succ.get_bit(&v - g_out_start)) {
-            delta_u += (path_counts[u] / path_counts[v]) * (1 + deltas[v]);
-          }
+    PrintStep("a", t.Seconds());
+    const NodeID* g_out_start = g.out_neigh(0).begin();
+    for (NodeID iter=0; iter < num_iters; iter++) {
+        NodeID source = sp.PickNext();
+        cout << "source: " << source << endl;
+        t.Start();
+        path_counts.fill(0);
+        depth_index.resize(0);
+        queue.reset();
+        succ.reset();
+        PBFS(g, source, path_counts, succ, depth_index, queue);
+        t.Stop();
+        PrintStep("b", t.Seconds());
+        pvector<ScoreT> deltas(g.num_nodes(), 0);
+        t.Start();
+        for (int d=depth_index.size()-2; d >= 0; d--) {
+#pragma omp parallel for num_threads(8)
+            for (auto it = depth_index[d]; it < depth_index[d+1]; it++) {
+                NodeID u = *it;
+                ScoreT delta_u = 0;
+                for (NodeID &v : g.out_neigh(u)) {
+                    if (succ.get_bit(&v - g_out_start)) {
+                        delta_u += (path_counts[u] / path_counts[v]) * (1 + deltas[v]);
+                    }
+                }
+                deltas[u] = delta_u;
+                scores[u] += delta_u;
+            }
         }
-        deltas[u] = delta_u;
-        scores[u] += delta_u;
-      }
+        t.Stop();
+        PrintStep("p", t.Seconds());
     }
-    t.Stop();
-    PrintStep("p", t.Seconds());
-  }
-  // normalize scores
-  ScoreT biggest_score = 0;
-  #pragma omp parallel for reduction(max : biggest_score)
-  for (NodeID n=0; n < g.num_nodes(); n++)
-    biggest_score = max(biggest_score, scores[n]);
-  #pragma omp parallel for
-  for (NodeID n=0; n < g.num_nodes(); n++)
-    scores[n] = scores[n] / biggest_score;
-  return scores;
+    // normalize scores
+    ScoreT biggest_score = 0;
+#pragma omp parallel for reduction(max : biggest_score)
+    for (NodeID n=0; n < g.num_nodes(); n++)
+        biggest_score = max(biggest_score, scores[n]);
+#pragma omp parallel for num_threads(8)
+    for (NodeID n=0; n < g.num_nodes(); n++)
+        scores[n] = scores[n] / biggest_score;
+    return scores;
 }
 
 
 void PrintTopScores(const Graph &g, const pvector<ScoreT> &scores) {
-  vector<pair<NodeID, ScoreT>> score_pairs(g.num_nodes());
-  for (NodeID n : g.vertices())
-    score_pairs[n] = make_pair(n, scores[n]);
-  int k = 5;
-  vector<pair<ScoreT, NodeID>> top_k = TopK(score_pairs, k);
-  for (auto kvp : top_k)
-    cout << kvp.second << ":" << kvp.first << endl;
+    vector<pair<NodeID, ScoreT>> score_pairs(g.num_nodes());
+    for (NodeID n : g.vertices())
+        score_pairs[n] = make_pair(n, scores[n]);
+    int k = 5;
+    vector<pair<ScoreT, NodeID>> top_k = TopK(score_pairs, k);
+    for (auto kvp : top_k)
+        cout << kvp.second << ":" << kvp.first << endl;
 }
 
 
@@ -164,84 +166,84 @@ void PrintTopScores(const Graph &g, const pvector<ScoreT> &scores) {
 // - regenerates successors from depths
 bool BCVerifier(const Graph &g, SourcePicker<Graph> &sp, NodeID num_iters,
                 const pvector<ScoreT> &scores_to_test) {
-  pvector<ScoreT> scores(g.num_nodes(), 0);
-  for (int iter=0; iter < num_iters; iter++) {
-    NodeID source = sp.PickNext();
-    // BFS phase, only records depth & path_counts
-    pvector<int> depths(g.num_nodes(), -1);
-    depths[source] = 0;
-    vector<CountT> path_counts(g.num_nodes(), 0);
-    path_counts[source] = 1;
-    vector<NodeID> to_visit;
-    to_visit.reserve(g.num_nodes());
-    to_visit.push_back(source);
-    for (auto it = to_visit.begin(); it != to_visit.end(); it++) {
-      NodeID u = *it;
-      for (NodeID v : g.out_neigh(u)) {
-        if (depths[v] == -1) {
-          depths[v] = depths[u] + 1;
-          to_visit.push_back(v);
+    pvector<ScoreT> scores(g.num_nodes(), 0);
+    for (int iter=0; iter < num_iters; iter++) {
+        NodeID source = sp.PickNext();
+        // BFS phase, only records depth & path_counts
+        pvector<int> depths(g.num_nodes(), -1);
+        depths[source] = 0;
+        vector<CountT> path_counts(g.num_nodes(), 0);
+        path_counts[source] = 1;
+        vector<NodeID> to_visit;
+        to_visit.reserve(g.num_nodes());
+        to_visit.push_back(source);
+        for (auto it = to_visit.begin(); it != to_visit.end(); it++) {
+            NodeID u = *it;
+            for (NodeID v : g.out_neigh(u)) {
+                if (depths[v] == -1) {
+                    depths[v] = depths[u] + 1;
+                    to_visit.push_back(v);
+                }
+                if (depths[v] == depths[u] + 1)
+                    path_counts[v] += path_counts[u];
+            }
         }
-        if (depths[v] == depths[u] + 1)
-          path_counts[v] += path_counts[u];
-      }
+        // Get lists of vertices at each depth
+        vector<vector<NodeID>> verts_at_depth;
+        for (NodeID n : g.vertices()) {
+            if (depths[n] != -1) {
+                if (depths[n] >= static_cast<int>(verts_at_depth.size()))
+                    verts_at_depth.resize(depths[n] + 1);
+                verts_at_depth[depths[n]].push_back(n);
+            }
+        }
+        // Going from farthest to clostest, compute "depencies" (deltas)
+        pvector<ScoreT> deltas(g.num_nodes(), 0);
+        for (int depth=verts_at_depth.size()-1; depth >= 0; depth--) {
+            for (NodeID u : verts_at_depth[depth]) {
+                for (NodeID v : g.out_neigh(u)) {
+                    if (depths[v] == depths[u] + 1) {
+                        deltas[u] += (path_counts[u] / path_counts[v]) * (1 + deltas[v]);
+                    }
+                }
+                scores[u] += deltas[u];
+            }
+        }
     }
-    // Get lists of vertices at each depth
-    vector<vector<NodeID>> verts_at_depth;
+    // Normalize scores
+    ScoreT biggest_score = *max_element(scores.begin(), scores.end());
+    for (NodeID n : g.vertices())
+        scores[n] = scores[n] / biggest_score;
+    // Compare scores
+    bool all_ok = true;
     for (NodeID n : g.vertices()) {
-      if (depths[n] != -1) {
-        if (depths[n] >= static_cast<int>(verts_at_depth.size()))
-          verts_at_depth.resize(depths[n] + 1);
-        verts_at_depth[depths[n]].push_back(n);
-      }
-    }
-    // Going from farthest to clostest, compute "depencies" (deltas)
-    pvector<ScoreT> deltas(g.num_nodes(), 0);
-    for (int depth=verts_at_depth.size()-1; depth >= 0; depth--) {
-      for (NodeID u : verts_at_depth[depth]) {
-        for (NodeID v : g.out_neigh(u)) {
-          if (depths[v] == depths[u] + 1) {
-            deltas[u] += (path_counts[u] / path_counts[v]) * (1 + deltas[v]);
-          }
+        ScoreT delta = abs(scores_to_test[n] - scores[n]);
+        if (delta > std::numeric_limits<ScoreT>::epsilon()) {
+            cout << n << ": " << scores[n] << " != " << scores_to_test[n];
+            cout << "(" << delta << ")" << endl;
+            all_ok = false;
         }
-        scores[u] += deltas[u];
-      }
     }
-  }
-  // Normalize scores
-  ScoreT biggest_score = *max_element(scores.begin(), scores.end());
-  for (NodeID n : g.vertices())
-    scores[n] = scores[n] / biggest_score;
-  // Compare scores
-  bool all_ok = true;
-  for (NodeID n : g.vertices()) {
-    ScoreT delta = abs(scores_to_test[n] - scores[n]);
-    if (delta > std::numeric_limits<ScoreT>::epsilon()) {
-      cout << n << ": " << scores[n] << " != " << scores_to_test[n];
-      cout << "(" << delta << ")" << endl;
-      all_ok = false;
-    }
-  }
-  return all_ok;
+    return all_ok;
 }
 
 
 int main(int argc, char* argv[]) {
-  CLIterApp cli(argc, argv, "betweenness-centrality", 1);
-  if (!cli.ParseArgs())
-    return -1;
-  if (cli.num_iters() > 1 && cli.start_vertex() != -1)
-    cout << "Warning: iterating from same source (-r & -i)" << endl;
-  Builder b(cli);
-  Graph g = b.MakeGraph();
-  SourcePicker<Graph> sp(g, cli.start_vertex());
-  auto BCBound =
-    [&sp, &cli] (const Graph &g) { return Brandes(g, sp, cli.num_iters()); };
-  SourcePicker<Graph> vsp(g, cli.start_vertex());
-  auto VerifierBound = [&vsp, &cli] (const Graph &g,
-                                     const pvector<ScoreT> &scores) {
-    return BCVerifier(g, vsp, cli.num_iters(), scores);
-  };
-  BenchmarkKernel(cli, g, BCBound, PrintTopScores, VerifierBound);
-  return 0;
+    CLIterApp cli(argc, argv, "betweenness-centrality", 1);
+    if (!cli.ParseArgs())
+        return -1;
+    if (cli.num_iters() > 1 && cli.start_vertex() != -1)
+        cout << "Warning: iterating from same source (-r & -i)" << endl;
+    Builder b(cli);
+    Graph g = b.MakeGraph();
+    SourcePicker<Graph> sp(g, cli.start_vertex());
+    auto BCBound =
+        [&sp, &cli] (const Graph &g) { return Brandes(g, sp, cli.num_iters()); };
+    SourcePicker<Graph> vsp(g, cli.start_vertex());
+    auto VerifierBound = [&vsp, &cli] (const Graph &g,
+                                      const pvector<ScoreT> &scores) {
+        return BCVerifier(g, vsp, cli.num_iters(), scores);
+    };
+    BenchmarkKernel(cli, g, BCBound, PrintTopScores, VerifierBound);
+    return 0;
 }
